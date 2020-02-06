badec5bb9bbfa324c05308241f723944a9931d73
Sidhra Cader <scader@mel0bhmac559.ap.corp.jwt.com>
2020-02-06 15:47:33 +1100

    validate data with yup

--- screens/BookForm.js
+++ screens/BookForm.js
@@ -2,12 +2,31 @@ import React from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import { globalStyles } from '../styles/global'
 import { Formik } from 'formik'
+import * as yup from 'yup'
+
+const bookSchema = yup.object({
+    title: yup
+            .string()
+            .required()
+            .min(4),
+    body: yup
+            .string()
+            .required()
+            .min(8),
+    rating: yup
+            .string()
+            .required()
+            .test('is-num-1-5', 'Rating must be a number 1-5', (val) => {
+                return parseInt(val) < 6 && parseInt(val) > 0
+            })
+})
 
 export default function BookForm({ addBook }) {
     return (
         <View style={ globalStyles.container }>
             <Formik
                 initialValues={ { title: '', body: '', rating: '' } }
+                validationSchema={ bookSchema }
                 onSubmit={ (values, actions) =>{
                     addBook(values)
                     actions.resetForm()
