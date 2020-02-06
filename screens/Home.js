eeff67cf31e8f8fb6cbda711fcbc1acb3a2dd20b
Sidhra Cader <scader@mel0bhmac559.ap.corp.jwt.com>
2020-02-06 13:19:24 +1100

    add form using formik

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react
 import { globalStyles } from '../styles/global'
 import { MaterialIcons } from '@expo/vector-icons'
 import Card from '../shared/Card'
+import BookForm from './BookForm'
 
 export default function Home({ navigation }) {
     const [ books, setBooks ] = useState([
@@ -16,9 +17,9 @@ export default function Home({ navigation }) {
     return(
         <View style={ globalStyles.container }>
             <Modal visible={ modalOpen } animationType="slide">
-                <View>
+                <View style={ styles.modalContent}>
                     <MaterialIcons name="close" size={ 24 } style={ {...styles.modalToggle, ...styles.modalClose} } onPress={ () => setModalOpen(false) } />
-                    <Text>This is the modal</Text>
+                    <BookForm />
                 </View>
 
             </Modal>
@@ -43,7 +44,7 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
     modalContent: {
-
+        flex: 1
     },
     modalToggle: {
         marginBottom: 10,
